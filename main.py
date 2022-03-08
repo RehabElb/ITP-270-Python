452d17e47fa324284ee2669565c063066e10eccf
ITP-270 <63158011+RehabElb@users.noreply.github.com>
2022-03-08 23:43:18 +0000

    code added

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print ("hello")
+my_name = "Rehab"
+print ("Hello " + my_name + "!")
+name = input("what is your name?")
+print ("my name is : " + name)
+
+
+alive = True
+if alive:
+  print("I am alive")
+else:
+  print('I am not')
